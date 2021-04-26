6d8bb83bd99104cfadfb02b42defffc8f3373e31
B00123722 <78503385+B00123722@users.noreply.github.com>
2021-04-26 03:35:33 +0100

    Update header.php

--- header.php
+++ header.php
@@ -1,16 +1,18 @@
-<?php session_start();
-if($_SESSION['Active'] == false){ /* Redirects user to Login.php if not logged in. Remember, we set $_SESSION['Active'] == true in login.php*/
-    header("location:login.php");
-    exit;
-}
-/*the code inside these php tags (i.e. the 5 lines of code above) are required for every page you wish to be accessible only after login*/
-?>
-
-
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Page Title</title>
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" href="../css/stylesheet.css">
+    <link rel="stylesheet" href="css/style.css" />
+ </head>
+<body>
+
+<div class="header">
+    <!--<img src="../template/aircraft.JPG" alt="logo " width="100" height="100">//--!>
+    <h1>AIR.</h1>
+    <h4>Simple Aircraft Trading Platform</h4>
+</div>
+
+</body>
+</html>

4803de4451de172dbe4bb395a40e80ea3981c4e2
B00123722 <78503385+B00123722@users.noreply.github.com>
2021-04-26 03:36:14 +0100

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,7 +1,15 @@
-<footer class="footer">
-    <p>Web dev serverside</p> <!-- code derived from https://github.com/mariofont -->
-</footer>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Page Title</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/style.css" />
+</head>
+<body>
 
+<div class="footer">
+    <h4>© Aircraft Registry</h4>
 </div>
 
 </body>
